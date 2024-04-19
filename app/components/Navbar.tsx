5548ea58a782e0e5156d87fb9b04cd1cc79324fc
Daniil Sychev <sych94@yandex.ru>
2024-04-19 03:18:58 +0300

    upd navBar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,10 +36,6 @@ const links = [
     titleRu: "Проекты",
     to: "/projects",
   },
-  // {
-  //   title: "Guestbook",
-  //   to: "/guestbook",
-  // }
 ];
 
 export default function Navbar() {
@@ -50,7 +46,7 @@ export default function Navbar() {
       {({ open }) => (
         <>
           <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justfiy-between h-16">
+            <div className="flex justfiy-between h-16 mt-2">
               <div className="flex justify-between w-full">
                 <div className="flex items-center">
                   <Link href="/">
@@ -91,19 +87,19 @@ export default function Navbar() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="pt-2 pb-3 space-y-1">
-              <Link
+              {/* <Link
                 href="/"
                 prefetch
               >
                 Home
-              </Link>
+              </Link> */}
               {links.map((link, idx) => (
                 <Link
                   key={idx}
                   href={link.to}
                   prefetch
                   className={`${
-                    pathname == "/"
+                    pathname === link.to
                       ? "bg-teal-50  border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
                       : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-teal-500 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
                   } `}
